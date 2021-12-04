50d435bab573b45e0ef908ef082f87cb17fdf537
Chaitanya7666 <chaitu.y25@gmail.com>
2021-12-04 10:15:27 +0530

    Added : database json file

--- src/components/Filter/styles.js
+++ src/components/Filter/styles.js
@@ -3,6 +3,8 @@ import { makeStyles } from "@material-ui/core";
 const useStyles = makeStyles({
   filterBody: {
     flex: 2,
+    border : "1px solid black",
+    height : "99vh"
   },
 });
 
--- src/components/ProductListing/styles.js
+++ src/components/ProductListing/styles.js
@@ -1,9 +1,10 @@
 import { makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles({
-    productListingBody:{
-        flex : 8
-    }
-})
+  productListingBody: {
+    flex: 8,
+    border: "1px solid black",
+  },
+});
 
 export default useStyles;
